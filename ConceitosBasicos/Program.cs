d18e9ad1f1b595a8566d922428b430a93ee0e95d
irvin <irvin@IRVINGJESSICA>
2023-08-22 20:12:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConceitosBasicos;
+
+
+int num1 = 20;
+int num2 = 11;
+int result;
+double res;
+
+Somador soma;
+soma = new Somador();
+result = soma.Soma(num1, num2);
+
+Console.WriteLine($"A soma dos números {num1} + {num2} é {result}");
+
+Console.WriteLine("A medida 1,76 metros corresponde a " + Conversor.MetrosMilimetros(1.76) +"  milimetros");
